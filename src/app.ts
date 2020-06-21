1508a8898e80749b2dd89473089caca8c322ab05
ivgomez <iv.gomez@hotmail.com>
2020-06-21 13:56:54 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello from typescript node");
+});
+
+app.listen(3000, () => console.log("Server running"));
