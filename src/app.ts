a5861dd8ad00d98a82233b52d33353bc84bb0464
ivgomez <iv.gomez@hotmail.com>
2020-06-21 19:45:32 -0500

    set build

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-const app = express();
-
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello from typescript node");
-});
-
-app.listen(5000, () => console.log("Server running"));
